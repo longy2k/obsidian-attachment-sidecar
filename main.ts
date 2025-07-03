8c5ebad35232fe2243705e74d3319e47a898771f
Long Huynh <longhuynh.y2k@gmail.com>
2025-07-02 21:07:45 -0400

    fix: rename sidecar now updates backlinks associated

--- main.ts
+++ main.ts
@@ -1,7 +1,5 @@
 import { App, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
 interface SideCarSettings {
 	toggleSideCarFiles: boolean;
 }
@@ -15,55 +13,6 @@ export default class SideCarPlugin extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		// const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-		// 	// Called when the user clicks the icon.
-		// 	new Notice('This is a notice!');
-		// });
-		// Perform additional things with the ribbon
-		// ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		// const statusBarItemEl = this.addStatusBarItem();
-		// statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		// this.addCommand({
-		// 	id: 'open-sample-modal-simple',
-		// 	name: 'Open sample modal (simple)',
-		// 	callback: () => {
-		// 		new SampleModal(this.app).open();
-		// 	}
-		// });
-		// This adds an editor command that can perform some operation on the current editor instance
-		// this.addCommand({
-		// 	id: 'sample-editor-command',
-		// 	name: 'Sample editor command',
-		// 	editorCallback: (editor: Editor, view: MarkdownView) => {
-		// 		console.log(editor.getSelection());
-		// 		editor.replaceSelection('Sample Editor Command');
-		// 	}
-		// });
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		// this.addCommand({
-		// 	id: 'open-sample-modal-complex',
-		// 	name: 'Open sample modal (complex)',
-		// 	checkCallback: (checking: boolean) => {
-		// 		// Conditions to check
-		// 		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-		// 		if (markdownView) {
-		// 			// If checking is true, we're simply "checking" if the command can be run.
-		// 			// If checking is false, then we want to actually perform the operation.
-		// 			if (!checking) {
-		// 				new SampleModal(this.app).open();
-		// 			}
-
-		// 			// This command will only show up in Command Palette when the check function returns true
-		// 			return true;
-		// 		}
-		// 	}
-		// });
 
 		this.registerEvent(
 			this.app.vault.on('create', async (file) => {
@@ -93,7 +42,7 @@ export default class SideCarPlugin extends Plugin {
 
 		this.registerEvent(
 		this.app.vault.on('rename', async (file, oldPath) => {
-			// Case 1: Handle non-markdown files being renamed (your existing logic)
+			// Case 1: Handle non-markdown files being renamed
 			if (file instanceof TFile && file.extension !== 'md') {
 			const oldSidecarPath = `${oldPath}.md.md`;
 			const newSidecarPath = `${file.path}.md.md`;
@@ -121,7 +70,7 @@ export default class SideCarPlugin extends Plugin {
 					.replace(new RegExp(`!\\[\\[${oldFilename}\\]\\]`, 'g'), `![[${newFilename}]]`);
 				
 				// Rename the sidecar file
-				await this.app.vault.rename(sidecarFile, newSidecarPath);
+				await this.app.fileManager.renameFile(sidecarFile, newSidecarPath);
 				
 				// Update the content with new filename references
 				const renamedSidecarFile = this.app.vault.getAbstractFileByPath(newSidecarPath);
@@ -135,7 +84,7 @@ export default class SideCarPlugin extends Plugin {
 			}
 			}
 			
-			// Case 2: Handle markdown sidecar files being renamed (NEW LOGIC)
+			// Case 2: Handle markdown sidecar files being renamed
 			else if (file instanceof TFile && file.extension === 'md' && oldPath.endsWith('.md.md')) {
 			// Check if this is a sidecar file by seeing if there's a corresponding main file
 			const oldMainFilePath = oldPath.slice(0, -6); // Remove '.md' extension
@@ -164,7 +113,7 @@ export default class SideCarPlugin extends Plugin {
 					.replace(new RegExp(`!\\[\\[${oldFilename}\\]\\]`, 'g'), `![[${newFilename}]]`);
 				
 				// Rename the main file to match the sidecar
-				await this.app.vault.rename(mainFile, newMainFilePath);
+				await this.app.fileManager.renameFile(mainFile, newMainFilePath);
 				
 				// Update the sidecar content with new filename references
 				await this.app.vault.modify(file, updatedContent);
@@ -205,12 +154,6 @@ export default class SideCarPlugin extends Plugin {
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SideCarSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			// console.log('click', evt);
-		});
-
 	}
 
 	onunload() {
@@ -225,22 +168,6 @@ export default class SideCarPlugin extends Plugin {
 		await this.saveData(this.settings);
 	}
 }
-
-// class SampleModal extends Modal {
-// 	constructor(app: App) {
-// 		super(app);
-// 	}
-
-// 	onOpen() {
-// 		const {contentEl} = this;
-// 		contentEl.setText('Woah!');
-// 	}
-
-// 	onClose() {
-// 		const {contentEl} = this;
-// 		contentEl.empty();
-// 	}
-// }
 
 class SideCarSettingTab extends PluginSettingTab {
 	plugin: SideCarPlugin;
@@ -260,21 +187,15 @@ class SideCarSettingTab extends PluginSettingTab {
 		containerEl.createEl('p', {text: 'Backup your vault before activating just in case you want to revert changes.'});
 
 		new Setting(containerEl)
-			.setName('Generate and Run Sidecar Files')
-			.setDesc('Toggle to create sidecar markdown files for all binary attachments in vault.')
-			.addToggle(toggle => toggle
-				.setValue(this.plugin.settings.toggleSideCarFiles)
-				.onChange(async (value) => {
-					this.plugin.settings.toggleSideCarFiles = value;
-					await this.plugin.saveSettings();
-					
-					if (value) {
-						await this.createSideCarFiles();
-					}
-					// Handle toggle state change
-					// You might want to show/hide other settings based on this toggle
-				}));
-	}
+		.setName('Generate Sidecar Files')
+		.setDesc('Create sidecar markdown files for all binary attachments in vault.')
+		.addButton(button => button
+			.setButtonText('Generate Sidecar Files')
+			.setCta() // This makes it a Call-to-Action styled button
+			.onClick(async () => {
+			await this.createSideCarFiles();
+			}));
+		}
 
 	// Updated createSideCarFiles method for creating sidecar markdown files
 	async createSideCarFiles() {
